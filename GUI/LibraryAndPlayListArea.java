bd5f9262b6ed0546068292cf0611df7a0f90f9c9
9731067 <9731067@ceit.local>
2019-06-22 02:13:22 +0430

    class LibraryAndPlayListArea costructor is being completed

--- GUI/LibraryAndPlayListArea.java
+++ GUI/LibraryAndPlayListArea.java
@@ -1,4 +1,14 @@
 package GUI;
 
-public class LibraryAndPlayListArea {
+import javax.swing.*;
+import java.awt.*;
+
+public class LibraryAndPlayListArea extends JPanel {
+    private ImageIcon icon = new ImageIcon("homeIcon.png");
+    private JLabel homeLabel = new JLabel(icon);
+    public LibraryAndPlayListArea(){
+        super();
+        this.setLayout(new BorderLayout());
+        this.add(homeLabel,BorderLayout.NORTH);
+    }
 }
