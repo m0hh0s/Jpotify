0a810b86d22d5722e6375ddcd1f899d38e154037
9731067 <9731067@ceit.local>
2019-06-22 01:19:23 +0430

    add costructor

--- GUI/JpotifyGUI.java
+++ GUI/JpotifyGUI.java
@@ -1,4 +1,19 @@
 package GUI;
 
-public class JpotifyGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class JpotifyGUI extends JFrame {
+    private final String WINDOWS_TITLE = "Jpotify";
+    private final int WIDTH = 1400, HEIGHT = 2265;
+    private final int X = 500, Y = 500;
+    public JpotifyGUI(){
+        super();
+        this.setTitle(WINDOWS_TITLE);
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH, HEIGHT);
+        this.setLocation(X, Y);
+        this.setVisible(true);
+    }
 }
