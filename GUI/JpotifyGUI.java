e4fa069899e3abe103fce2a75836c3030055363b
9731067 <9731067@ceit.local>
2019-06-23 20:06:43 +0430

    created Friend Class just to use the empty methods

--- GUI/JpotifyGUI.java
+++ GUI/JpotifyGUI.java
@@ -11,16 +11,21 @@ public class JpotifyGUI extends JFrame {
     private final int X = 70, Y = 70;
     private LibraryAndPlayListArea libraryAndPlayListArea = new LibraryAndPlayListArea();
     private MusicPlayerArea musicPlayerArea = new MusicPlayerArea();
-    private JScrollPane scrollPane = new JScrollPane(libraryAndPlayListArea);
+    private FriendListArea friendListArea = new FriendListArea();
+    private JScrollPane friendScrollPane = new JScrollPane(friendListArea);
+    private JScrollPane libraryScrollPane = new JScrollPane(libraryAndPlayListArea);
     public JpotifyGUI() throws IOException {
         super();
+        friendScrollPane.setColumnHeaderView(new JLabel("Friend Activity"));
+        friendScrollPane.setPreferredSize(new Dimension(140,500));
         this.setTitle(WINDOWS_TITLE);
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WIDTH, HEIGHT);
         this.setLocation(X, Y);
-        this.add(scrollPane,BorderLayout.WEST);
+        this.add(libraryScrollPane,BorderLayout.WEST);
         this.add(musicPlayerArea,BorderLayout.SOUTH);
+        this.add(friendScrollPane,BorderLayout.EAST);
         this.setVisible(true);
     }
 }
