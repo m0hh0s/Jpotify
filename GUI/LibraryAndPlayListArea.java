aea49963f3f0e67eaf1728d3e3d30dc61422673b
9731067 <9731067@ceit.local>
2019-06-23 12:00:39 +0430

    put button instead of label in someplaces also add method instead of a lot of code

--- GUI/LibraryAndPlayListArea.java
+++ GUI/LibraryAndPlayListArea.java
@@ -6,55 +6,64 @@ import java.awt.*;
 public class LibraryAndPlayListArea extends JPanel {
     private final int EMPTY_SPACE = 14;
     private PlayList playList = new PlayList();
+    private BoxLayout boxLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
     private ImageIcon homeIcon = new ImageIcon("homeIcon.png");
     private ImageIcon plusIcon = new ImageIcon("plusIcon.png");
-    private JLabel homeLabel = new JLabel();
-    private JLabel plusLabelForSong = new JLabel();
-    private JLabel plusLabelForPlayList = new JLabel();
+    private JButton homeButton = new JButton();
+    private JButton plusLabelForSong = new JButton();
+    private JButton plusLabelForPlayList = new JButton();
     private JLabel yourLibraryHead = new JLabel();
-    private JLabel recentlyPlayed = new JLabel();
-    private JLabel songs = new JLabel();
-    private JLabel albums = new JLabel();
-    private JLabel artist = new JLabel();
-    private JLabel playListLabel = new JLabel();
+    private JButton recentlyPlayed = new JButton();
+    private JButton songs = new JButton();
+    private JButton albums = new JButton();
+    private JButton artist = new JButton();
+    private JButton playListButton = new JButton();
     private JScrollPane playListScroll = new JScrollPane(playList);
-
-
-    private BoxLayout boxLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
     public LibraryAndPlayListArea(){
         super();
-        homeLabel.setIcon(homeIcon);
-        homeLabel.setText("Home");
-        plusLabelForSong.setIcon(plusIcon);
-        plusLabelForSong.setText("Add Song");
-        plusLabelForPlayList.setIcon(plusIcon);
-        plusLabelForPlayList.setText("Add PlayList");
-        yourLibraryHead.setText("YOUR LIBRARY");
-        recentlyPlayed.setText("RecentlyPlayed");
-        songs.setText("Songs");
-        albums.setText("Albums");
-        artist.setText("Artist");
-        playListLabel.setText("PlayList");
+        prepareButtonToAdd(homeButton,homeIcon,"Home");
+        prepareButtonToAdd(plusLabelForSong,plusIcon,"Add Song");
+        prepareButtonToAdd(plusLabelForPlayList,plusIcon,"Add PlayList");
+        prepareButtonToAdd(recentlyPlayed,"RecentlyPlayed");
+        prepareLabelToAdd(yourLibraryHead,"YOUR LIBRARY");
+        prepareButtonToAdd(songs,"Songs");
+        prepareButtonToAdd(albums,"Albums");
+        prepareButtonToAdd(artist,"Artist");
+        prepareButtonToAdd(playListButton,"PlayList");
+        prepareScrollToAdd(playListScroll,"PLAYLIST");
 
-        // add 4 space to make them all align together
-        playListScroll.setColumnHeaderView(new JLabel("    PLAYLIST"));
         this.setLayout(boxLayout);
-        this.add(homeLabel);
-        this.add(Box.createRigidArea(new Dimension(EMPTY_SPACE*2,EMPTY_SPACE*2)));
+        this.add(homeButton);
+        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
         this.add(yourLibraryHead);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE/2)));
         this.add(recentlyPlayed);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
         this.add(songs);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
-        this.add(playListLabel);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
+        this.add(playListButton);
         this.add(albums);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
         this.add(artist);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
         this.add(plusLabelForSong);
         this.add(playListScroll);
         this.add(plusLabelForPlayList);
+    }
+    private void prepareButtonToAdd(JButton btn , ImageIcon icon, String name){
+        btn.setIcon(icon);
+        btn.setText(name);
+        btn.setBorderPainted(false);
+        btn.setFocusPainted(false);
+        btn.setBackground(Color.WHITE);
+    }
+    private void prepareButtonToAdd(JButton btn, String name){
+        btn.setText(name);
+        btn.setBorderPainted(false);
+        btn.setFocusPainted(false);
+        btn.setBackground(Color.WHITE);
+    }
+    private void prepareLabelToAdd(JLabel label, String name){
+        label.setText(name);
+    }
+    private void prepareScrollToAdd(JScrollPane scrollPane, String name){
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setColumnHeaderView(new JLabel(name));
     }
 }
