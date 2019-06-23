efe3fe00f96ca74af9fd056b113300100cfb8f59
9731067 <9731067@ceit.local>
2019-06-23 14:22:43 +0430

    add buttons and soundSlider

--- GUI/MusicPlayerArea.java
+++ GUI/MusicPlayerArea.java
@@ -4,12 +4,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MusicPlayerArea extends JPanel{
+    private final int BUTTON_SIZE = 24;
     private JSlider timeSlider = new JSlider();
-    private JButton play = new JButton();
+    private JSlider soundSlider = new JSlider();
+    private JButton playButton = new JButton();
+    private JButton forwardButton = new JButton();
+    private JButton backwardButton = new JButton();
+    private ImageIcon backwardIcon = new ImageIcon("backwardIcon.png");
+    private ImageIcon playIcon = new ImageIcon("playIcon.png");
+    private ImageIcon forwardIcon = new ImageIcon("forwardIcon.png");
+    private JPanel northPanel = new JPanel(new BorderLayout());
+    private JPanel buttonPanel = new JPanel(new FlowLayout());
     public MusicPlayerArea(){
         super();
-//        timeSlider.set
         this.setLayout(new BorderLayout());
-        this.add(timeSlider);
+        soundSlider.setPreferredSize(new Dimension(70,0));
+        prepareButtonToAdd(backwardButton,backwardIcon);
+        prepareButtonToAdd(playButton,playIcon);
+        prepareButtonToAdd(forwardButton,forwardIcon);
+        buttonPanel.add(backwardButton);
+        buttonPanel.add(playButton);
+        buttonPanel.add(forwardButton);
+        northPanel.add(buttonPanel,BorderLayout.CENTER);
+        northPanel.add(soundSlider,BorderLayout.EAST);
+        this.add(northPanel,BorderLayout.CENTER);
+        this.add(timeSlider,BorderLayout.SOUTH);
+    }
+    private void prepareButtonToAdd(JButton btn ,ImageIcon icon){
+        btn.setIcon(icon);
+        btn.setPreferredSize(new Dimension(BUTTON_SIZE,BUTTON_SIZE));
     }
 }
