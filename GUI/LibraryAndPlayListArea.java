64cb0e3ea5c67140b5449643f774339685fbb667
9731067 <9731067@ceit.local>
2019-06-23 13:21:03 +0430

    added gridLayout in the BoxLayout

--- GUI/LibraryAndPlayListArea.java
+++ GUI/LibraryAndPlayListArea.java
@@ -19,6 +19,11 @@ public class LibraryAndPlayListArea extends JPanel {
     private JButton artist = new JButton();
     private JButton playListButton = new JButton();
     private JScrollPane playListScroll = new JScrollPane(playList);
+    private JPanel firstGridPanel = new JPanel(new GridLayout(0,1));
+    private JPanel firstAndHalfGridPanel = new JPanel(new GridLayout(0,1));
+    private JPanel secondGridPanel = new JPanel(new GridLayout(0,1));
+    private JPanel thirdGridPanel = new JPanel(new GridLayout(0,1));
+
     public LibraryAndPlayListArea(){
         super();
         prepareButtonToAdd(homeButton,homeIcon,"Home");
@@ -33,17 +38,37 @@ public class LibraryAndPlayListArea extends JPanel {
         prepareScrollToAdd(playListScroll,"PLAYLIST");
 
         this.setLayout(boxLayout);
-        this.add(homeButton);
-        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
-        this.add(yourLibraryHead);
-        this.add(recentlyPlayed);
-        this.add(songs);
-        this.add(playListButton);
-        this.add(albums);
-        this.add(artist);
-        this.add(plusLabelForSong);
+//        this.add(homeButton);
+        firstGridPanel.setMaximumSize(new Dimension(140,20));
+        firstAndHalfGridPanel.setMaximumSize(new Dimension(140,20));
+        secondGridPanel.setMaximumSize(new Dimension(140,20));
+        thirdGridPanel.setMaximumSize(new Dimension(140,20));
+        firstGridPanel.add(homeButton);
+        this.add(firstGridPanel);
+//        this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
+        firstAndHalfGridPanel.add(yourLibraryHead);
+//        this.add(yourLibraryHead);
+        this.add(firstAndHalfGridPanel);
+        secondGridPanel.add(recentlyPlayed);
+        secondGridPanel.add(songs);
+        secondGridPanel.add(playListButton);
+        secondGridPanel.add(albums);
+        secondGridPanel.add(artist);
+        secondGridPanel.add(plusLabelForSong);
+        this.add(secondGridPanel);
+
+
+//        this.add(recentlyPlayed);
+//        this.add(songs);
+//        this.add(playListButton);
+//        this.add(albums);
+//        this.add(artist);
+//        this.add(plusLabelForSong);
         this.add(playListScroll);
-        this.add(plusLabelForPlayList);
+//        this.add(plusLabelForPlayList);
+        thirdGridPanel.add(plusLabelForPlayList);
+        this.add(thirdGridPanel);
+
     }
     private void prepareButtonToAdd(JButton btn , ImageIcon icon, String name){
         btn.setIcon(icon);
@@ -51,12 +76,14 @@ public class LibraryAndPlayListArea extends JPanel {
         btn.setBorderPainted(false);
         btn.setFocusPainted(false);
         btn.setBackground(Color.WHITE);
+        btn.setHorizontalAlignment(SwingConstants.LEFT);
     }
     private void prepareButtonToAdd(JButton btn, String name){
         btn.setText(name);
         btn.setBorderPainted(false);
         btn.setFocusPainted(false);
         btn.setBackground(Color.WHITE);
+        btn.setPreferredSize(new Dimension(70,25));
     }
     private void prepareLabelToAdd(JLabel label, String name){
         label.setText(name);
