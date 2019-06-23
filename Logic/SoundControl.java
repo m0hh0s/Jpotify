ddf53e7b6b14237429c34d9a13f738098a7327d6
9731016 <9731016@ceit.local>
2019-06-24 04:00:20 +0430

    SoundControl can now mute and unmute the system

---
+++ Logic/SoundControl.java
@@ -0,0 +1,49 @@
+package Logic;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+
+public class SoundControl {
+    public static void setSystemVolume(int volume) {
+        if(volume < 0 || volume > 100)
+        {
+            throw new RuntimeException("Error: " + volume + " is not a valid number. Choose a number between 0 and 100");
+        }
+
+        else
+        {
+            double endVolume = 655.35 * volume;
+
+            Runtime rt = Runtime.getRuntime();
+            Process pr;
+            try
+            {
+                pr = rt.exec("src\\Logic\\nircmd.exe" + " setsysvolume " + endVolume);
+                pr = rt.exec("src\\Logic\\nircmd.exe" + " mutesysvolume 0");
+
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void mute(){
+        Runtime rt = Runtime.getRuntime();
+        Process pr;
+        try
+        {
+            pr = rt.exec("src\\Logic\\nircmd.exe" + " mutesysvolume 2");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
