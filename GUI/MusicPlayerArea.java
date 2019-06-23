dba557557aed5fe9e7be45f2103ac2b258ea0273
9731067 <9731067@ceit.local>
2019-06-23 12:01:11 +0430

    put button instead of label in someplaces also add method instead of a lot of code

--- GUI/MusicPlayerArea.java
+++ GUI/MusicPlayerArea.java
@@ -1,4 +1,15 @@
 package GUI;
 
-public class MusicPlayerArea {
+import javax.swing.*;
+import java.awt.*;
+
+public class MusicPlayerArea extends JPanel{
+    private JSlider timeSlider = new JSlider();
+    private JButton play = new JButton();
+    public MusicPlayerArea(){
+        super();
+//        timeSlider.set
+        this.setLayout(new BorderLayout());
+        this.add(timeSlider);
+    }
 }
