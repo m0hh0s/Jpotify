442c9a4d99f21ddfc64fb1226147892e48689aee
9731067 <9731067@ceit.local>
2019-06-23 22:31:19 +0430

    class PlayList created

--- GUI/PlayList.java
+++ GUI/PlayList.java
@@ -1,24 +1,4 @@
 package GUI;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-
-public class PlayList extends JPanel {
-    private final int EMPTY_SPACE = 14;
-    private BoxLayout boxLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
-    private ArrayList<JLabel> playList = new ArrayList<>();
-    public PlayList(){
-        super();
-        this.setLayout(boxLayout);
-        for(JLabel pl:playList){
-            this.add(pl);
-            this.add(Box.createRigidArea(new Dimension(0,EMPTY_SPACE)));
-        }
-    }
-    public void addPlayList(String name){
-        JLabel temp = new JLabel(name);
-        this.add(temp);
-        playList.add(temp);
-    }
+public class PlayList {
 }
