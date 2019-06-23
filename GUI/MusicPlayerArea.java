9257702c8fd7135d2fafa2bf835dfd73e01af90c
9731067 <9731067@ceit.local>
2019-06-23 17:24:46 +0430

    added the label of time

--- GUI/MusicPlayerArea.java
+++ GUI/MusicPlayerArea.java
@@ -2,6 +2,7 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class MusicPlayerArea extends JPanel{
     private final int BUTTON_SIZE = 24;
@@ -13,9 +14,13 @@ public class MusicPlayerArea extends JPanel{
     private ImageIcon backwardIcon = new ImageIcon("backwardIcon.png");
     private ImageIcon playIcon = new ImageIcon("playIcon.png");
     private ImageIcon forwardIcon = new ImageIcon("forwardIcon.png");
+    private JLabel timePassed = new JLabel("00:00:00");
+    private JLabel timeRemaining = new JLabel("00:00:00");
+    private JLabel img = new JLabel();
     private JPanel northPanel = new JPanel(new BorderLayout());
     private JPanel buttonPanel = new JPanel(new FlowLayout());
-    public MusicPlayerArea(){
+    private JPanel southPanel = new JPanel(new BorderLayout());
+    public MusicPlayerArea() throws IOException {
         super();
         this.setLayout(new BorderLayout());
         soundSlider.setPreferredSize(new Dimension(70,0));
@@ -27,11 +32,15 @@ public class MusicPlayerArea extends JPanel{
         buttonPanel.add(forwardButton);
         northPanel.add(buttonPanel,BorderLayout.CENTER);
         northPanel.add(soundSlider,BorderLayout.EAST);
+        southPanel.add(timeSlider,BorderLayout.CENTER);
+        southPanel.add(timePassed,BorderLayout.WEST);
+        southPanel.add(timeRemaining,BorderLayout.EAST);
         this.add(northPanel,BorderLayout.CENTER);
-        this.add(timeSlider,BorderLayout.SOUTH);
+        this.add(southPanel,BorderLayout.SOUTH);
     }
     private void prepareButtonToAdd(JButton btn ,ImageIcon icon){
         btn.setIcon(icon);
         btn.setPreferredSize(new Dimension(BUTTON_SIZE,BUTTON_SIZE));
     }
+
 }
