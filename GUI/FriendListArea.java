492c81ed45dfb467bb491391880e022b7b85beb6
9731067 <9731067@ceit.local>
2019-06-23 20:06:43 +0430

    created Friend Class just to use the empty methods

--- GUI/FriendListArea.java
+++ GUI/FriendListArea.java
@@ -1,4 +1,17 @@
 package GUI;
 
-public class FriendListArea {
+import javax.swing.*;
+import java.awt.*;
+
+public class FriendListArea extends JPanel {
+    private GridLayout gridLayout = new GridLayout(0,1);
+    private JButton imaginaryFriend = new JButton();
+    public FriendListArea(){
+        super();
+        this.setMaximumSize(new Dimension(140,50));
+        this.setLayout(gridLayout);
+    }
+    private void prepareButtonToAdd(Friend friend){
+
+    }
 }
