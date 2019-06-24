03ff813ef643955baccef69388b10831f2b382cc
9731067 <9731067@ceit.local>
2019-06-24 14:29:36 +0430

    two permanent playList added

--- GUI/PlayLists.java
+++ GUI/PlayLists.java
@@ -6,15 +6,30 @@ import java.util.ArrayList;
 
 public class PlayLists extends JPanel {
     private final int EMPTY_SPACE = 14;
-    private ArrayList<PlayList> playLists = new ArrayList<PlayList>();
+    private final PlayList favourite = new PlayList("Favourite");
+    private final PlayList sharedSongs = new PlayList("shared songs");
+    private ArrayList<PlayList> playLists = new ArrayList<>();
     private BoxLayout boxLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
-    private ArrayList<JButton> songs = new ArrayList<>();
     public PlayLists(){
         super();
         this.setLayout(boxLayout);
-
+        addPlayList(favourite);
+        addPlayList(sharedSongs);
+        addPlayListButton();
     }
-    public void addSongToPlayList(Song song){
-        this.add();
+    public void addPlayList(PlayList playList){
+        playLists.add(playList);
+    }
+    private void addPlayListButton(){
+        for(PlayList playList:playLists){
+            JPanel gridPanel = new JPanel(new GridLayout(1,1));
+            gridPanel.setMaximumSize(new Dimension(140,40));
+            JButton temp = new JButton(playList.getName());
+            temp.setFocusPainted(false);
+            temp.setBorderPainted(false);
+            temp.setBackground(Color.WHITE);
+            gridPanel.add(temp);
+            this.add(gridPanel);
+        }
     }
 }
