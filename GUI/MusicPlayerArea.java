adca9cc936625ee522034ce96b30c10a72bd4362
9731067 <9731067@ceit.local>
2019-06-24 15:56:14 +0430

    removed image from this section

--- GUI/MusicPlayerArea.java
+++ GUI/MusicPlayerArea.java
@@ -1,11 +1,7 @@
 package GUI;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class MusicPlayerArea extends JPanel{
     private final int BUTTON_SIZE = 24;
@@ -21,18 +17,15 @@ public class MusicPlayerArea extends JPanel{
     private ImageIcon plusIcon = new ImageIcon("plusIcon.png");
     private JLabel timePassed = new JLabel("00:00:00");
     private JLabel timeRemaining = new JLabel("00:00:00");
-    private JLabel img = new JLabel();
     private JPanel northPanel = new JPanel(new BorderLayout());
     private JPanel buttonPanel = new JPanel(new FlowLayout());
     private JPanel southPanel = new JPanel(new BorderLayout());
-    private JPanel westPanel = new JPanel(new BorderLayout());
     private JPanel centerPanel = new JPanel(new BorderLayout());
     private JPanel southOfNorthPanel = new JPanel(new BorderLayout());
-    public MusicPlayerArea() throws IOException {
+    public MusicPlayerArea()  {
         super();
         this.setLayout(new BorderLayout());
         soundSlider.setPreferredSize(new Dimension(70,0));
-        setImageIcon("SongImage.png",img);
         prepareButtonToAdd(backwardButton,backwardIcon);
         prepareButtonToAdd(playButton,playIcon);
         prepareButtonToAdd(forwardButton,forwardIcon);
@@ -49,20 +42,11 @@ public class MusicPlayerArea extends JPanel{
         southPanel.add(timeRemaining,BorderLayout.EAST);
         centerPanel.add(southOfNorthPanel,BorderLayout.CENTER);
         centerPanel.add(southPanel,BorderLayout.SOUTH);
-        westPanel.add(img);
         this.add(centerPanel,BorderLayout.CENTER);
-        this.add(westPanel,BorderLayout.WEST);
     }
     private void prepareButtonToAdd(JButton btn ,ImageIcon icon){
         btn.setIcon(icon);
         btn.setPreferredSize(new Dimension(BUTTON_SIZE,BUTTON_SIZE));
     }
-    private void setImageIcon(String path, JLabel label) throws IOException {
-        BufferedImage img = ImageIO.read(new File(path));
-        BufferedImage finalImg = new BufferedImage(140,140,img.getType());
-        Graphics2D graphics2D = finalImg.createGraphics();
-        graphics2D.drawImage(img,0,0,140,140,null);
-        graphics2D.dispose();
-        label.setIcon(new ImageIcon(finalImg));
-    }
+
 }
