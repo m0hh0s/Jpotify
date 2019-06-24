6892ac2120cc1cedacae418d2a9c9dff4b1dccf1
9731067 <9731067@ceit.local>
2019-06-24 15:55:49 +0430

    let the west part stretch into the south

--- GUI/JpotifyGUI.java
+++ GUI/JpotifyGUI.java
@@ -12,20 +12,23 @@ public class JpotifyGUI extends JFrame {
     private LibraryAndPlayListArea libraryAndPlayListArea = new LibraryAndPlayListArea();
     private MusicPlayerArea musicPlayerArea = new MusicPlayerArea();
     private FriendListArea friendListArea = new FriendListArea();
+    private JPanel centerEastAndSouthPanel = new JPanel(new BorderLayout());
     private JScrollPane friendScrollPane = new JScrollPane(friendListArea);
     private JScrollPane libraryScrollPane = new JScrollPane(libraryAndPlayListArea);
     public JpotifyGUI() throws IOException {
         super();
         friendScrollPane.setColumnHeaderView(new JLabel("Friend Activity"));
         friendScrollPane.setPreferredSize(new Dimension(140,500));
+        friendScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         this.setTitle(WINDOWS_TITLE);
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WIDTH, HEIGHT);
         this.setLocation(X, Y);
+        centerEastAndSouthPanel.add(musicPlayerArea,BorderLayout.SOUTH);
+        centerEastAndSouthPanel.add(friendScrollPane,BorderLayout.EAST);
         this.add(libraryScrollPane,BorderLayout.WEST);
-        this.add(musicPlayerArea,BorderLayout.SOUTH);
-        this.add(friendScrollPane,BorderLayout.EAST);
+        this.add(centerEastAndSouthPanel,BorderLayout.CENTER);
         this.setVisible(true);
     }
 }
