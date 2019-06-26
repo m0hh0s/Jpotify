676002b6d4273ff3a7eb1ca1e261b682e0ee2dae
9731016 <9731016@ceit.local>
2019-06-27 03:49:55 +0430

    user class now can return the current user

--- Logic/User.java
+++ Logic/User.java
@@ -3,6 +3,16 @@ package Logic;
 import java.io.Serializable;
 
 public class User implements Serializable {
+    private static User instance = null;
+
+    private static User getInstance(){
+        return instance;
+    }
+
+    public static void setInstance(User instance) {
+        User.instance = instance;
+    }
+
     private String username;
     private MusicLibrary musicLibrary = new MusicLibrary();
 
@@ -17,4 +27,6 @@ public class User implements Serializable {
     public MusicLibrary getMusicLibrary() {
         return musicLibrary;
     }
+
+
 }
