c6079c7ec9446497a5b2263400f8fbf04814e520
9731067 <9731067@ceit.local>
2019-06-26 16:43:53 +0430

    online icon changed

--- GUI/FriendListArea.java
+++ GUI/FriendListArea.java
@@ -1,17 +1,21 @@
 package GUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FriendListArea extends JPanel {
     private BoxLayout boxLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
     private ArrayList<JButton> friendsButtons = new ArrayList<>();
     private ArrayList<Friend> friends = new ArrayList<>();
-    private ImageIcon onlineIcon = new ImageIcon("onlineIcon.png");
+    private ImageIcon onlineIcon = new ImageIcon("Icons/onlineIcon.png");
     private Color NEAR_BLACK = new Color(28,28,28);
 
-    public FriendListArea(){
+    public FriendListArea() throws IOException {
         super();
         this.setLayout(boxLayout);
         addFriend(new Friend("sattar","my lovely song"));
@@ -23,7 +27,7 @@ public class FriendListArea extends JPanel {
         addFriend(tmp);
         prepareButtonToAdd(friends);
     }
-    private void prepareButtonToAdd(ArrayList<Friend> friends){
+    private void prepareButtonToAdd(ArrayList<Friend> friends) throws IOException {
         for(Friend frnd:friends) {
             JButton temp = new JButton();
             JPanel tmp = new JPanel(new GridLayout(0,1));
@@ -31,7 +35,7 @@ public class FriendListArea extends JPanel {
             if (frnd.isOnline()) {
                 String text = frnd.getName() + "\n" + frnd.getSong();
                 temp.setText("<html>" + text.replaceAll("\\n", "<br>") + "</html>");
-                temp.setIcon(onlineIcon);
+                setImageIconForButton("Icons/onlineIcon.png",temp,18);
             } else {
                 String text = frnd.getName() + "  " + frnd.getTime() + "\n" + frnd.getSong();
                 temp.setText("<html>" + text.replaceAll("\\n", "<br>") + "</html>");
@@ -44,6 +48,11 @@ public class FriendListArea extends JPanel {
             this.add(tmp);
             friendsButtons.add(temp);
         }
+    }
+    private void setImageIconForButton(String path, JButton btn ,int size) throws IOException {
+        BufferedImage img = ImageIO.read(new File(path));
+        Image image = img.getScaledInstance(size,size,Image.SCALE_SMOOTH);
+        btn.setIcon(new ImageIcon(image));
     }
     private void addFriend(Friend friend){
         friends.add(friend);
