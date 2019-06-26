57a8885056e665e0648f627647f679cbd0a017d2
9731067 <9731067@ceit.local>
2019-06-26 20:48:39 +0430

    changed the color and corrected the layout

--- GUI/CenterArea.java
+++ GUI/CenterArea.java
@@ -10,36 +10,42 @@ import java.util.ArrayList;
 
 public class CenterArea extends JPanel{
     private ArrayList<PlayList> playLists ;
-    private JPanel northPanel = new JPanel(new BorderLayout());
-    private JPanel northWestPanel = new JPanel(new BorderLayout());
-    private JPanel flowLayoutPanel = new JPanel(new FlowLayout(0,2,2));
+    private JPanel flowLayoutPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
+    private Color VERY_DARK_GRAY = new Color(40,40,40);
     public CenterArea(ArrayList<PlayList> playLists) throws IOException {
         super();
         this.setLayout(new BorderLayout());
         this.playLists = playLists;
-        northWestPanel.add(flowLayoutPanel);
-        northPanel.add(northWestPanel,BorderLayout.WEST);
-        this.add(northPanel,BorderLayout.NORTH);
+        this.setBackground(VERY_DARK_GRAY);
+        flowLayoutPanel.setBackground(VERY_DARK_GRAY);
         preparePlayListsToAdd();
+        this.add(flowLayoutPanel);
     }
     private void preparePlayListsToAdd() throws IOException {
         for(PlayList playList:playLists) {
-            JPanel motherGridPanel = new JPanel();
-            motherGridPanel.setLayout(new BoxLayout(motherGridPanel,BoxLayout.PAGE_AXIS));
+            JPanel motherGridPanel = new JPanel(new BorderLayout());
+//            motherGridPanel.setLayout(new BoxLayout(motherGridPanel,BoxLayout.PAGE_AXIS));
             JPanel buttonPanel = new JPanel(new GridLayout(1,1));
             JPanel labelPanel = new JPanel(new GridLayout(1,1));
             JLabel label = new JLabel(playList.getName());
             JButton btn = new JButton();
+            motherGridPanel.setBackground(VERY_DARK_GRAY);
+            buttonPanel.setBackground(VERY_DARK_GRAY);
+            labelPanel.setBackground(VERY_DARK_GRAY);
+            label.setForeground(Color.WHITE);
+            label.setFont(new Font("optima",Font.BOLD,12));
+            motherGridPanel.setMaximumSize(new Dimension(200,250));
             buttonPanel.setMaximumSize(new Dimension(200,200));
+            btn.setPreferredSize(new Dimension(205,205));
             labelPanel.setMaximumSize(new Dimension(200,50));
             btn.setSize(new Dimension(200,200));
             btn.setFocusPainted(false);
             btn.setBorderPainted(false);
-            this.setImageIcon("Icons/anonymousMusicIcon.png", btn,200);
+            this.setImageIcon("Icons/anonymousMusicIcon.jpg", btn,200);
             buttonPanel.add(btn);
             labelPanel.add(label);
-            motherGridPanel.add(buttonPanel);
-            motherGridPanel.add(labelPanel);
+            motherGridPanel.add(buttonPanel,BorderLayout.NORTH);
+            motherGridPanel.add(labelPanel,BorderLayout.SOUTH);
             flowLayoutPanel.add(motherGridPanel,FlowLayout.LEFT);
         }
 
