a53d093e51cf932846acb6dd54285948f6da3e9f
9731067 <9731067@ceit.local>
2019-06-26 20:49:23 +0430

    added a method to increase the quality of images

--- GUI/MusicPlayerArea.java
+++ GUI/MusicPlayerArea.java
@@ -26,7 +26,7 @@ public class MusicPlayerArea extends JPanel{
     private JPanel westPanel = new JPanel(new BorderLayout());
     private JPanel centerPanel = new JPanel(new BorderLayout());
     private JPanel gridSongInfo = new JPanel(new GridLayout(2,1));
-    private Color NEAR_VERY_DARK_GRAY = new Color(70,70,70);
+    private Color NEAR_VERY_DARK_GRAY = new Color(50,50,50);
 
 
     public MusicPlayerArea() throws IOException {
@@ -89,10 +89,79 @@ public class MusicPlayerArea extends JPanel{
     }
     private void setImageIconForButton(String path, JButton btn ,int size) throws IOException {
         BufferedImage img = ImageIO.read(new File(path));
-        Image image = img.getScaledInstance(size,size,Image.SCALE_SMOOTH);
-        btn.setIcon(new ImageIcon(image));
+        BufferedImage finalImg = getScaledInstance(img,size,size,RenderingHints.VALUE_INTERPOLATION_BICUBIC,true);
+        Graphics2D graphics2D = finalImg.createGraphics();
+        graphics2D.drawImage(img,0,0,size,size,null);
+        graphics2D.dispose();
+        btn.setIcon(new ImageIcon(finalImg));
     }
+    /**
+     * Convenience method that returns a scaled instance of the
+     * provided {@code BufferedImage}.
+     *
+     * @param img the original image to be scaled
+     * @param targetWidth the desired width of the scaled instance,
+     *    in pixels
+     * @param targetHeight the desired height of the scaled instance,
+     *    in pixels
+     * @param hint one of the rendering hints that corresponds to
+     *    {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     *    {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     *    {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     *    {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality if true, this method will use a multi-step
+     *    scaling technique that provides higher quality than the usual
+     *    one-step technique (only useful in downscaling cases, where
+     *    {@code targetWidth} or {@code targetHeight} is
+     *    smaller than the original dimensions, and generally only when
+     *    the {@code BILINEAR} hint is specified)
+     * @return a scaled version of the original {@code BufferedImage}
+     */
+    public BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, Object hint, boolean higherQuality)
+    {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ?
+                BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage)img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
 
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
+    }
     private Font addFont(String path,float size)  {
         Font customFont = null;
         try {
