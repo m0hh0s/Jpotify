68797bde07c7d75e70912d1cf0fca84a1ffd8ce0
9731067 <9731067@ceit.local>
2019-06-27 00:55:36 +0430

    method needed added

--- GUI/Friend.java
+++ GUI/Friend.java
@@ -1,4 +1,41 @@
 package GUI;
 
 public class Friend {
+    private String name;
+
+    private boolean isOnline = true;
+
+    private int time;
+
+    //this should become Song in future
+    private String song;
+
+    public Friend(String name, String song) {
+        this.name = name;
+        this.song = song;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public int getTime() {
+        return time;
+    }
 }
