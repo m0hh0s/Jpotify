ee46363c40ddcbc130ff618ef53504ba67b8b6ed
9731067 <9731067@ceit.local>
2019-06-27 00:53:33 +0430

    added font to some text and change background

--- GUI/JpotifyGUI.java
+++ GUI/JpotifyGUI.java
@@ -2,7 +2,10 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.basic.BasicScrollBarUI;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -13,29 +16,53 @@ public class JpotifyGUI extends JFrame {
     private LibraryAndPlayListArea libraryAndPlayListArea = new LibraryAndPlayListArea();
     private MusicPlayerArea musicPlayerArea = new MusicPlayerArea();
     private FriendListArea friendListArea = new FriendListArea();
-    private JPanel centerEastAndSouthPanel = new JPanel(new BorderLayout());
     private JScrollPane friendScrollPane = new JScrollPane(friendListArea);
     private JScrollPane libraryScrollPane = new JScrollPane(libraryAndPlayListArea);
     private ArrayList<PlayList> playLists = new ArrayList<>();
     private CenterArea centerArea ;
+    private JLabel friendAreaHeader = new JLabel("Friend Activity");
+    private JPanel eastArea = new JPanel(new BorderLayout());
+    private Color NEAR_BLACK = new Color(28,28,28);
     public JpotifyGUI() throws IOException, FontFormatException {
         super();
         playLists = libraryAndPlayListArea.getPlayLists().getPlayLists();
         centerArea = new CenterArea(playLists);
-        friendScrollPane.setColumnHeaderView(new JLabel("Friend Activity"));
         friendScrollPane.setPreferredSize(new Dimension(140,500));
         friendScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        friendScrollPane.setBackground(NEAR_BLACK);
+        libraryScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        UIManager.put("ScrollBar.thumb", new ColorUIResource(NEAR_BLACK));
+        libraryScrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() );
+        friendScrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() );
+        eastArea.setBackground(NEAR_BLACK);
+        Font friendAreaHeaderfont = addFont("Fonts/Proza_Libre/ProzaLibre-SemiBoldItalic.ttf",12);
+        friendAreaHeader.setForeground(Color.WHITE);
+        friendAreaHeader.setFont(friendAreaHeaderfont);
+        eastArea.add(friendAreaHeader,BorderLayout.NORTH);
+        eastArea.add(friendScrollPane,BorderLayout.CENTER);
         this.setTitle(WINDOWS_TITLE);
-        this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WIDTH, HEIGHT);
         this.setLocation(X, Y);
-        centerEastAndSouthPanel.add(musicPlayerArea,BorderLayout.SOUTH);
-        centerEastAndSouthPanel.add(friendScrollPane,BorderLayout.EAST);
-        centerEastAndSouthPanel.add(centerArea);
+        this.setLayout(new BorderLayout());
+        this.add(musicPlayerArea,BorderLayout.SOUTH);
+        this.add(eastArea,BorderLayout.EAST);
+        this.add(centerArea);
         this.add(libraryScrollPane,BorderLayout.WEST);
-        this.add(centerEastAndSouthPanel,BorderLayout.CENTER);
-
         this.setVisible(true);
+    }
+    private Font addFont(String path,float size)  {
+        Font customFont = null;
+        try {
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File(path)).deriveFont(size);
+        } catch (FontFormatException e) {
+            System.out.println("the font wan not there");
+        } catch (IOException e) {
+            System.out.println("the font wan not there");
+        }
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        //register the font
+        ge.registerFont(customFont);
+        return customFont;
     }
 }
