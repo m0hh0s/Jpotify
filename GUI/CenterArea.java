27e8b3c7fecec671dcec52e1154ee11103446b27
9731067 <9731067@ceit.local>
2019-06-28 01:30:26 +0430

    changed the look to list

--- GUI/CenterArea.java
+++ GUI/CenterArea.java
@@ -1,7 +1,5 @@
 package GUI;
 
-import Logic.User;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -12,24 +10,15 @@ import java.util.ArrayList;
 
 public class CenterArea extends JPanel{
     private ArrayList<PlayList> playLists ;
-    private JPanel flowLayoutPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,5,5));
-    private JPanel northPanel = new JPanel(new BorderLayout());
-    private JLabel name = new JLabel("rideman");
     private Color VERY_DARK_GRAY = new Color(40,40,40);
-    private Color NEAR_BLACK = new Color(28,28,28);
+    private Color NEAR_VERY_DARK_GRAY = new Color(50,50,50);
 
     public CenterArea(ArrayList<PlayList> playLists) throws IOException {
         super();
-        name.setForeground(Color.WHITE);
-        northPanel.add(name,BorderLayout.EAST);
-        northPanel.setBackground(NEAR_BLACK);
-        this.setLayout(new BorderLayout());
+        this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
         this.playLists = playLists;
         this.setBackground(VERY_DARK_GRAY);
-        flowLayoutPanel.setBackground(VERY_DARK_GRAY);
         preparePlayListsToAdd();
-        this.add(flowLayoutPanel);
-        this.add(northPanel,BorderLayout.NORTH);
     }
     private void preparePlayListsToAdd() throws IOException {
 
@@ -37,34 +26,70 @@ public class CenterArea extends JPanel{
         //  label title
 
         for(PlayList playList:playLists) {
-            JPanel motherGridPanel = new JPanel(new BorderLayout());
-//            motherGridPanel.setLayout(new BoxLayout(motherGridPanel,BoxLayout.PAGE_AXIS));
-            JPanel buttonPanel = new JPanel(new GridLayout(1,1));
-            JPanel labelPanel = new JPanel(new GridLayout(1,1));
-            JLabel label = new JLabel(playList.getName());
-            JButton btn = new JButton();
-            motherGridPanel.setBackground(VERY_DARK_GRAY);
-            buttonPanel.setBackground(VERY_DARK_GRAY);
-            labelPanel.setBackground(VERY_DARK_GRAY);
-            label.setForeground(Color.WHITE);
-            label.setFont(new Font("optima",Font.BOLD,12));
-            motherGridPanel.setMaximumSize(new Dimension(200,250));
-            buttonPanel.setMaximumSize(new Dimension(200,200));
-            btn.setPreferredSize(new Dimension(205,205));
-            labelPanel.setMaximumSize(new Dimension(200,50));
-            btn.setSize(new Dimension(200,200));
-            btn.setFocusPainted(false);
-            btn.setBorderPainted(false);
-            this.setImageIcon("Icons/anonymousMusicIcon.jpg", btn,200);
-            buttonPanel.add(btn);
-            labelPanel.add(label);
-            motherGridPanel.add(buttonPanel,BorderLayout.NORTH);
-            motherGridPanel.add(labelPanel,BorderLayout.SOUTH);
-            flowLayoutPanel.add(motherGridPanel,FlowLayout.LEFT);
+            JButton listButton = new JButton();
+            JButton deleteButton = new JButton();
+            listButton.setLayout(new BorderLayout());
+
+            JPanel labelPanel = new JPanel(new BorderLayout());
+            JPanel motherPanel = new JPanel(new BorderLayout());
+            JLabel topLabel = new JLabel();
+            JLabel bottomLabel = new JLabel();
+            JLabel artWork = new JLabel();
+            Font topLabelFont = new Font("optima",Font.PLAIN,14);
+            Font bottomLabelFont = new Font("optima",Font.BOLD,12);
+
+            setImageIconForLabel("Icons/anonymousMusicIcon.png",artWork,50);
+
+            listButton.setMaximumSize(new Dimension(700,70));
+            listButton.setBorderPainted(false);
+            listButton.setFocusPainted(false);
+
+            setImageIconForButton("Icons/crossIcon.png",deleteButton,28);
+
+            deleteButton.setMaximumSize(new Dimension(28,28));
+            deleteButton.setBorderPainted(false);
+            deleteButton.setFocusPainted(false);
+            deleteButton.setBackground(NEAR_VERY_DARK_GRAY);
+
+            //must add space before the song and artist name
+            topLabel.setText("   Your Safe with me");
+            topLabel.setFont(topLabelFont);
+            bottomLabel.setText("   Sheriff");
+            bottomLabel.setFont(bottomLabelFont);
+            topLabel.setForeground(Color.WHITE);
+            bottomLabel.setForeground(Color.WHITE);
+
+            labelPanel.add(topLabel,BorderLayout.NORTH);
+            labelPanel.add(bottomLabel,BorderLayout.SOUTH);
+
+            motherPanel.add(labelPanel,BorderLayout.WEST);
+            listButton.add(motherPanel);
+            listButton.add(artWork,BorderLayout.WEST);
+            listButton.add(deleteButton,BorderLayout.EAST);
+            motherPanel.setBackground(NEAR_VERY_DARK_GRAY);
+            labelPanel.setBackground(NEAR_VERY_DARK_GRAY);
+            listButton.setBackground(NEAR_VERY_DARK_GRAY);
+            this.add(Box.createRigidArea(new Dimension(40,10)));
+
+            this.add(listButton);
         }
 
     }
-    private void setImageIcon(String path, JButton button,int size)  {
+    private void setImageIconForLabel(String path, JLabel label, int size)  {
+        //duplicate code
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            System.out.println("the path is wrong and btw im in centerArea");
+        }
+        BufferedImage finalImg = new BufferedImage(size,size,img.getType());
+        Graphics2D graphics2D = finalImg.createGraphics();
+        graphics2D.drawImage(img,0,0,size,size,null);
+        graphics2D.dispose();
+        label.setIcon(new ImageIcon(finalImg));
+    }
+    private void setImageIconForButton(String path, JButton button,int size)  {
         //duplicate code
         BufferedImage img = null;
         try {
