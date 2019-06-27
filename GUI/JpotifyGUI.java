d7350a7d2b2a0dc9bf1686588700e0ca7dc55de7
9731067 <9731067@ceit.local>
2019-06-28 01:31:01 +0430

    add id in north of this also put center into scrollPane

--- GUI/JpotifyGUI.java
+++ GUI/JpotifyGUI.java
@@ -13,20 +13,30 @@ public class JpotifyGUI extends JFrame {
     private final String WINDOWS_TITLE = "Jpotify";
     private final int WIDTH = 1132, HEIGHT = 700;
     private final int X = 70, Y = 70;
+    private ArrayList<PlayList> playLists;
     private LibraryAndPlayListArea libraryAndPlayListArea = new LibraryAndPlayListArea();
     private MusicPlayerArea musicPlayerArea = new MusicPlayerArea();
     private FriendListArea friendListArea = new FriendListArea();
+    private CenterArea centerArea ;
     private JScrollPane friendScrollPane = new JScrollPane(friendListArea);
     private JScrollPane libraryScrollPane = new JScrollPane(libraryAndPlayListArea);
-    private ArrayList<PlayList> playLists = new ArrayList<>();
-    private CenterArea centerArea ;
+    private JScrollPane centerScrollPane = new JScrollPane();
     private JLabel friendAreaHeader = new JLabel("Friend Activity");
+
+    //must add space after it
+    private JLabel id = new JLabel("97310**  ");
     private JPanel eastArea = new JPanel(new BorderLayout());
+    private JPanel centerAndWestArea = new JPanel(new BorderLayout());
+    private JPanel idPanel = new JPanel(new BorderLayout());
     private Color NEAR_BLACK = new Color(28,28,28);
+    private Color NEAR_VERY_DARK_GRAY = new Color(50,50,50);
     public JpotifyGUI() throws IOException, FontFormatException {
         super();
         playLists = libraryAndPlayListArea.getPlayLists().getPlayLists();
         centerArea = new CenterArea(playLists);
+        centerScrollPane.setViewportView(centerArea);
+        UIManager.put("ScrollBar.thumb", new ColorUIResource(NEAR_VERY_DARK_GRAY));
+        centerScrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() );
         friendScrollPane.setPreferredSize(new Dimension(140,500));
         friendScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         friendScrollPane.setBackground(NEAR_BLACK);
@@ -35,11 +45,18 @@ public class JpotifyGUI extends JFrame {
         libraryScrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() );
         friendScrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() );
         eastArea.setBackground(NEAR_BLACK);
-        Font friendAreaHeaderfont = addFont("Fonts/Proza_Libre/ProzaLibre-SemiBoldItalic.ttf",12);
+        Font friendAreaHeaderFont = addFont("Fonts/Proza_Libre/ProzaLibre-SemiBoldItalic.ttf",12);
         friendAreaHeader.setForeground(Color.WHITE);
-        friendAreaHeader.setFont(friendAreaHeaderfont);
+        friendAreaHeader.setFont(friendAreaHeaderFont);
         eastArea.add(friendAreaHeader,BorderLayout.NORTH);
         eastArea.add(friendScrollPane,BorderLayout.CENTER);
+
+        id.setForeground(Color.WHITE);
+        idPanel.add(id,BorderLayout.EAST);
+        idPanel.setBackground(NEAR_BLACK);
+        centerAndWestArea.add(idPanel,BorderLayout.NORTH);
+        centerAndWestArea.add(centerScrollPane);
+        centerAndWestArea.add(libraryScrollPane,BorderLayout.WEST);
         this.setTitle(WINDOWS_TITLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WIDTH, HEIGHT);
@@ -47,8 +64,8 @@ public class JpotifyGUI extends JFrame {
         this.setLayout(new BorderLayout());
         this.add(musicPlayerArea,BorderLayout.SOUTH);
         this.add(eastArea,BorderLayout.EAST);
-        this.add(centerArea);
-        this.add(libraryScrollPane,BorderLayout.WEST);
+        this.add(centerAndWestArea);
+        this.setVisible(true);
     }
     private Font addFont(String path,float size)  {
         Font customFont = null;
