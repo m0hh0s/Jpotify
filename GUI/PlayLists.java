e888e7f3c7ea18d3404a111b3c951a63947d3c83
9731016 <9731016@ceit.local>
2019-06-28 12:18:59 +0430

    left-side playlist updated

--- GUI/PlayLists.java
+++ GUI/PlayLists.java
@@ -1,29 +1,26 @@
 package GUI;
 
+import Logic.Playlist;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class PlayLists extends JPanel {
-    private final PlayList favourite = new PlayList("Favourite");
-    private final PlayList sharedSongs = new PlayList("Shared Playlist");
-    private ArrayList<PlayList> playLists = new ArrayList<>();
+    private ArrayList<Playlist> playLists = new ArrayList<>();
     private Color NEAR_BLACK = new Color(28,28,28);
     private BoxLayout boxLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
 
     public PlayLists(){
         super();
         this.setLayout(boxLayout);
-        addPlayList(favourite);
-        addPlayList(sharedSongs);
-        addPlayListButton();
         this.setBackground(NEAR_BLACK);
     }
-    public void addPlayList(PlayList playList){
+    public void addPlayList(Playlist playList){
         playLists.add(playList);
+        addPlayListButton();
     }
     private void addPlayListButton(){
-        for(PlayList playList:playLists){
+        for(Playlist playList:playLists){
             JPanel gridPanel = new JPanel(new GridLayout(1,1));
             gridPanel.setMaximumSize(new Dimension(140,40));
             JButton btn = new JButton(playList.getName());
@@ -37,7 +34,9 @@ public class PlayLists extends JPanel {
             this.add(gridPanel);
         }
     }
-    public ArrayList<PlayList> getPlayLists() {
-        return playLists;
+
+    public void setPlatlists(ArrayList<Playlist> playlists) {
+        this.playLists = playlists;
+        addPlayListButton();
     }
 }
